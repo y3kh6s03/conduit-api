91a4b7b38f13e1f15a3ea82ee915ff5ec8f1482a
yosuke026 <y3kh6s03@gmail.com>
2024-02-27 14:54:43 +0900

    create register api

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
+});
+
+Route::prefix('api/users')
+->controller(AuthController::class)
+->name('users.')
+->group(function(){
+    Route::post('/','register')->name('register');
 });
